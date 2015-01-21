d81ebce00621d9726abd5175e2abc2218e74a38a
Matt Kilgore <mattkilgore12@gmail.com>
2015-01-20 21:18:49 -0500

    Add timestamp to debug output

--- nclyr/debug.c
+++ nclyr/debug.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
 
 #include "debug.h"
 
@@ -21,10 +22,16 @@ void debug_close(void)
 
 void debug_printf(const char *file, const char *func, const char *line, const char *str, ...)
 {
+    char timestamp[21] = { 0 };
+    time_t tim;
     va_list args;
 
+    tim = time(NULL);
+
+    strftime(timestamp, sizeof(timestamp), "%F %T", localtime(&tim));
+
     va_start(args, str);
-    fprintf(debugfile, "%s: %s: %s: ", file, func, line);
+    fprintf(debugfile, "[%s] %s: %s: %s: ", timestamp, file, func, line);
     vfprintf(debugfile, str, args);
     va_end(args);
 
