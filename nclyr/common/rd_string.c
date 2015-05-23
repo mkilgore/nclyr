3629a2896b7281ebec56a79a0e697326ace9ea82
Matt Kilgore <mattkilgore12@gmail.com>
2015-05-23 18:06:39 -0400

    Prevent double-free from rd_string
    In general this wouldn't be an issue, but because rd_string_dup will
    free the rd_string if it's currently allocated, we have to manually set
    it to NULL on free (As well as check if it's already NULL) to make sure
    we don't attempt to free the rd_string if we free an rd_string, and then
    attempt to dup another string into it at some-point.

--- nclyr/common/rd_string.c
+++ nclyr/common/rd_string.c
@@ -6,8 +6,12 @@
 
 void rd_string_free(struct rd_string *str)
 {
-    if (str->was_malloced)
-        free(str->str);
+    if (str->was_malloced) {
+        if (str->str) {
+            free(str->str);
+            str->str = NULL;
+        }
+    }
 }
 
 void rd_string_dup(struct rd_string *str, const char *s)
